0900bc8b25d4a3a9a7608294a0214a236377deac
Michael Lai <mike24567@gmail.com>
2016-10-03 09:54:11 -0400

    Trouble solving adding to the pack

--- test.rb
+++ test.rb
@@ -15,6 +15,28 @@ end
 
 pumba =  Animal.new("Pumba")
 
+class Lion < Animal
+  attr_accessor :king
+  @@pack = 0
+  @@pack += 1
+
+
+  def is_king
+    if name == "Simba"
+      @king = true
+    else
+      @king = false
+    end
+  end
+
+  def self.pack
+    @@pack
+  end
+end
+
+simba = Lion.new("Simba")
+nala = Lion.new("Nala")
+
 
 
 binding.pry
