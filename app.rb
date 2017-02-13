a42d97c343a512e921c315d03ffb03559f0171e7
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-02-13 09:32:44 -0500

    Answered question 3

--- app.rb
+++ app.rb
@@ -1,23 +1,30 @@
 require "pry"
-
+# Q1
 # def offer_rose(person)
 # puts "Would you take this rose, #{person}, in exchange for giving an old beggar woman shelter from the bitter cold?"
 # end
 #
 # offer_rose("young prince")
 
+# Q2
+# town = {
+#   residents: ["Maurice", "Belle", "Gaston"],
+#   castle: {
+#     num_rooms: 47,
+#     residents: "Robby Benson",
+#     guests: []
+#   }
+# }
+#
+# belle = town[:residents].slice!(1)
+# town[:castle][:guests].push(belle)
 
-town = {
-  residents: ["Maurice", "Belle", "Gaston"],
-  castle: {
-    num_rooms: 47,
-    residents: "Robby Benson",
-    guests: []
-  }
-}
-
-belle = town[:residents].slice!(1)
-town[:castle][:guests].push(belle)
-
+# Q3
+# 
+# friends = ["Chip Potts", "Cogsworth", "Lumière", "Mrs. Potts"]
+#
+# friends.each do |friend|
+#   puts "Belle is friends with #{friend}"
+# end
 
 binding.pry
