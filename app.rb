bef348507be0b55c69404d4ba10b1d7355c2c198
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-02-13 09:58:05 -0500

    checkpoint 04

---
+++ app.rb
@@ -0,0 +1,46 @@
+require "pry"
+
+# Question 1
+
+def offer_rose (person)
+  offer_rose(young prince)
+  puts "Would you take this rose" + person, "in exchange for giving an old beggar woman shelter from the bitter cold?"
+end
+
+# Question 2
+#
+# town = {
+#   residents: ["Maurice", "Belle", "Gaston"],
+#   castle: {
+#     num_rooms: 47,
+#     residents: "Robby Benson",
+#     guests: []
+#   }
+# }
+#
+# a = residents
+# a.delete(Belle)
+# guests.add_person (Belle)
+# #
+# # # Quesetion 3
+# #
+# friends = [
+#   {
+#     name: "Chip Potts",
+#   },
+#   {
+#     name: "Cogsworth",
+#   },
+#   {
+#     name: "Lumière",
+#   {
+#     name: "Mrs. Potts"
+#   }]
+#
+# while
+#   friends.each do |friend|
+#     puts "Belle is friends with " + friends[:name] + "?"
+# end
+
+binding.pry
+puts "end of file"
